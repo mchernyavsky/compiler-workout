333b83b6f9f225352d3df8175f3ec442168ec39b
misha <chemike47@gmail.com>
2018-02-26 11:45:32 +0300

    Implement eval function

--- src/Expr.ml
+++ src/Expr.ml
@@ -3,10 +3,10 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-             
-(* The type for the expression. Note, in regular OCaml there is no "@type..." 
-   notation, it came from GT. 
+open GT
+
+(* The type for the expression. Note, in regular OCaml there is no "@type..."
+   notation, it came from GT.
 *)
 @type expr =
   (* integer constant *) | Const of int
@@ -27,28 +27,49 @@ type state = string -> int
 (* Empty state: maps every variable into nothing. *)
 let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-(* Update: non-destructively "modifies" the state s by binding the variable x 
+(* Update: non-destructively "modifies" the state s by binding the variable x
    to value v and returns the new state.
 *)
 let update x v s = fun y -> if x = y then v else s y
 
-(* An example of a non-trivial state: *)                                                   
+(* An example of a non-trivial state: *)
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(* let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+    ) ["x"; "a"; "y"; "z"; "t"; "b"] *)
 
 (* Expression evaluator
 
      val eval : state -> expr -> int
- 
-   Takes a state and an expression, and returns the value of the expression in 
+
+   Takes a state and an expression, and returns the value of the expression in
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval state expr = match expr with
+  | Const c -> c
+  | Var v -> state v
+  | Binop (op, expr1, expr2) ->
+    let int2bool i = i <> 0 in
+    let bool2int b = if b then 1 else 0 in
+    let lhs = eval state expr1 in
+    let rhs = eval state expr2 in
+    match op with
+      | "+" -> lhs + rhs
+      | "-" -> lhs - rhs
+      | "*" -> lhs * rhs
+      | "/" -> lhs / rhs
+      | "%" -> lhs mod rhs
+      | "<" -> bool2int (lhs < rhs)
+      | "<=" -> bool2int (lhs <= rhs)
+      | ">" -> bool2int (lhs > rhs)
+      | ">=" -> bool2int (lhs >= rhs)
+      | "==" -> bool2int (lhs = rhs)
+      | "!=" -> bool2int (lhs <> rhs)
+      | "&&" -> bool2int ((int2bool lhs) && (int2bool rhs))
+      | "!!" -> bool2int ((int2bool lhs) || (int2bool rhs))
+      | _ -> failwith (Printf.sprintf "Undefined operator %s" op)

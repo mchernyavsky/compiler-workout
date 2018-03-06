9723b3cf7ef3897777512c89466522670d140ed0
misha <chemike47@gmail.com>
2018-03-06 12:29:44 +0300

    Implement compile function

--- src/SM.ml
+++ src/SM.ml
@@ -1,15 +1,16 @@
-open GT       
-       
+open GT
+open List
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -22,8 +23,24 @@ type config = int list * Syntax.Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+let eval config prg =
+  let eval_step (stack, ((state, input, output) as config)) insn =
+    match insn with
+      | BINOP op ->
+        let rhs :: lhs :: stack' = stack in
+        let lhs = Syntax.Expr.Const lhs in
+        let rhs = Syntax.Expr.Const rhs in
+        let expr = Syntax.Expr.Binop (op, lhs, rhs) in
+        let result = Syntax.Expr.eval state expr in
+        (result :: stack', config)
+      | CONST c -> (c :: stack, config)
+      | READ -> (hd input :: stack, (state, tl input, output))
+      | WRITE -> (tl stack, (state, input, output @ [hd stack]))
+      | LD v -> (state v :: stack, config)
+      | ST v -> (tl stack, (Syntax.Expr.update v (hd stack) state, input, output))
+      | _ -> failwith "Unknown instruction"
+  in fold_left eval_step config prg
 
 (* Stack machine compiler
 
@@ -33,4 +50,14 @@ let eval _ = failwith "Not yet implemented"
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+let rec compile stmt =
+  let rec compile_expr expr = match expr with
+    | Syntax.Expr.Const c -> [CONST c]
+    | Syntax.Expr.Var v -> [LD v]
+    | Syntax.Expr.Binop (op, lhs, rhs) ->
+      compile_expr lhs @ compile_expr rhs @ [BINOP op]
+  in match stmt with
+    | Syntax.Stmt.Read v -> [READ; ST v]
+    | Syntax.Stmt.Write expr -> compile_expr expr @ [WRITE]
+    | Syntax.Stmt.Assign (v, expr) -> compile_expr expr @ [ST v]
+    | Syntax.Stmt.Seq (prg1, prg2) -> compile prg1 @ compile prg2
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -1,14 +1,15 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-    
+open GT
+open List
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -22,14 +23,14 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
@@ -37,14 +38,36 @@ module Expr =
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval state expr = match expr with
+      | Const c -> c
+      | Var v -> state v
+      | Binop (op, expr1, expr2) ->
+        let int2bool i = i <> 0 in
+        let bool2int b = if b then 1 else 0 in
+        let lhs = eval state expr1 in
+        let rhs = eval state expr2 in
+        match op with
+          | "+" -> lhs + rhs
+          | "-" -> lhs - rhs
+          | "*" -> lhs * rhs
+          | "/" -> lhs / rhs
+          | "%" -> lhs mod rhs
+          | "<" -> bool2int (lhs < rhs)
+          | "<=" -> bool2int (lhs <= rhs)
+          | ">" -> bool2int (lhs > rhs)
+          | ">=" -> bool2int (lhs >= rhs)
+          | "==" -> bool2int (lhs = rhs)
+          | "!=" -> bool2int (lhs <> rhs)
+          | "&&" -> bool2int ((int2bool lhs) && (int2bool rhs))
+          | "!!" -> bool2int ((int2bool lhs) || (int2bool rhs))
+          | _ -> failwith (Printf.sprintf "Undefined operator %s" op)
 
   end
-                    
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -57,7 +80,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -65,6 +88,10 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
-                                                         
+    let rec eval ((state, input, output) as config) stmt = match stmt with
+      | Read v -> (Expr.update v (hd input) state, tl input, output)
+      | Write expr -> (state, input, Expr.eval state expr :: output)
+      | Assign (v, expr) -> (Expr.update v (Expr.eval state expr) state, input, output)
+      | Seq (stmt1, stmt2) -> eval (eval config stmt1) stmt2
+
   end
